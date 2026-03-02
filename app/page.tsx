09a0e29b185d4a1d8959d8046e3e6f3c53ab000b
Emmanuel Ochieng <eochieng16@gmail.com>
2026-03-02 12:11:18 +0300

    finalize home page layout & functionality

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,9 @@ import { useRouter } from "next/navigation";
 import type { Game } from "@/types/game";
 import type { Decisions } from "@/lib/simulation/types";
 
+// utils
+import { formatNumberWithCommas } from "@/utils/formattedCurrecy";
+
 function OfficeVisualization({ engineers, sales }: { engineers: number, sales: number }) {
   const desks = [...Array(engineers).fill('engineer'), ...Array(sales).fill('sales')]
 
@@ -17,7 +20,7 @@ function OfficeVisualization({ engineers, sales }: { engineers: number, sales: n
 
   return (
     <div style={{ marginTop: 24 }}>
-      <h2>Office</h2>
+      <h2 className="text-[22px] font-medium">Office</h2>
 
       <div style={{ display: 'grid', gridTemplateColumns: 'repeat(5, 40px)', gap: 8 }}>
         {desks.map((role, i) => (
@@ -59,6 +62,11 @@ export default function Home() {
     isWin = game.year >= 10 && game.cash > 0
   }
 
+  async function loadHistory() {
+    const { data: history } = await supabase.from('quarter_history').select('*').order('created_at', { ascending: false }).limit(4)
+
+    setHistory(history ?? [])
+  }
 
 
   async function advanceQuarter() {
@@ -84,10 +92,13 @@ export default function Home() {
     // reset hires
     setDecisions({ ...decisions, new_engineers: 0, new_sales_staff: 0 })
 
+    loadHistory()
+
     setSubmitting(false)
   }
 
-  // load game every time route changes
+
+  // load game & quarter history every time route changes
   useEffect(() => {
     async function loadGame() {
       const { data: { user } } = await supabase.auth.getUser()
@@ -124,11 +135,7 @@ export default function Home() {
       setLoading(false)
     }
 
-    async function loadHistory() {
-      const { data: history } = await supabase.from('quarter_history').select('*').order('created_at', { ascending: false }).limit(4)
 
-      setHistory(history ?? [])
-    }
 
     loadHistory()
     loadGame()
@@ -139,54 +146,57 @@ export default function Home() {
   if (loading) return <p>Loading...</p>
 
   return (
-    <div>
-      {history.length > 0 && (
-        <div className="mt-12">
-          <h2 className="font-semibold text-xl">Last 4 Quarters</h2>
+    <div className="flex">
 
-          <div className="rounded-xl border border-[#ffffff46] inline-block overflow-x-auto">
-            <table className="">
-              <thead className="bg-[#4747474b] ">
-                <tr className="font-medium pb-5 border-b border-[#ffffff46] text-[#ffffffd8]">
-                  <th className="px-6 py-3">Year</th>
-                  <th className="px-6 py-3">Quarter</th>
-                  <th className="px-6 py-3">Revenue</th>
-                  <th className="px-6 py-3">Net Income</th>
-                  <th className="px-6 py-3">Cash</th>
-                </tr>
-              </thead>
-              <tbody>
-                {history.map((q, i) => (
-                  <tr className="text-[#ffffffc3] border-[#ffffff46]" key={q.id}>
-                    <td className={`${i !== history.length - 1 && 'border-b border-[#ffffff46]'} px-6 py-4`}>{q.year}</td>
-                    <td className={`${i !== history.length - 1 && 'border-b border-[#ffffff46]'} px-6 py-4`}> {q.quarter}</td>
-                    <td className={`${i !== history.length - 1 && 'border-b border-[#ffffff46]'} px-6 py-4`}> {q.revenue}</td>
-                    <td className={`${i !== history.length - 1 && 'border-b border-[#ffffff46]'} px-6 py-4`}> {q.net_income}</td>
-                    <td className={`${i !== history.length - 1 && 'border-b border-[#ffffff46]'} px-6 py-4`}> {q.cash_end}</td>
+      <div className="w-[50vw]">
+        {history.length > 0 && (
+          <div className="mt-12 flex flex-col items-center">
+            <h2 className="font-semibold text-xl mb-4">Last 4 Quarters</h2>
+
+            <div className="rounded-xl border border-[#ffffff46] inline-block overflow-x-auto ">
+              <table className="">
+                <thead className="bg-[#47474771] ">
+                  <tr className="font-medium pb-5 border-b border-[#ffffff46] text-[#ffffffd8]">
+                    <th className="px-6 py-3">Year</th>
+                    <th className="px-6 py-3">Quarter</th>
+                    <th className="px-6 py-3">Revenue</th>
+                    <th className="px-6 py-3">Net Income</th>
+                    <th className="px-6 py-3">Cash</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {history.map((q, i) => (
+                    <tr className="text-[#ffffffc3] border-[#ffffff46]" key={q.id}>
+                      <td className={`${i !== history.length - 1 && 'border-b border-[#ffffff46]'} px-6 py-4`}>{q.year}</td>
+                      <td className={`${i !== history.length - 1 && 'border-b border-[#ffffff46]'} px-6 py-4`}> {q.quarter}</td>
+                      <td className={`${i !== history.length - 1 && 'border-b border-[#ffffff46]'} px-6 py-4`}> ${formatNumberWithCommas(q.revenue)}</td>
+                      <td className={`${i !== history.length - 1 && 'border-b border-[#ffffff46]'} px-6 py-4`}> ${formatNumberWithCommas(q.net_income)}</td>
+                      <td className={`${i !== history.length - 1 && 'border-b border-[#ffffff46]'} px-6 py-4`}> ${formatNumberWithCommas(q.cash_end)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
+        )}
+        <div className="flex justify-center gap-16 items-center">
+          {
+            game && (
+              <div>
+                <h1 className="text-[22px] font-medium border-b py-1 mb-4">Startup Dashboard</h1>
+                <p>Cash: ${formatNumberWithCommas(game.cash)}</p>
+                <p>Year {game.year}, Quarter {game.quarter}</p>
+                <p>Engineers: {game.engineers}</p>
+                <p>Sales Staff: {game.sales_staff}</p>
+              </div>
+            )
+          }
+          {game && (
+            <OfficeVisualization engineers={game?.engineers} sales={game?.sales_staff} />
+          )}
         </div>
-      )}
-      <h1>Startup Dashboard</h1>
-      {
-        game && (
-          <>
-            <p>Cash: ${game.cash}</p>
-            <p>Year {game.year}, Quarter {game.quarter}</p>
-            <p>Engineers: {game.engineers}</p>
-            <p>Sales Staff: {game.sales_staff}</p>
-          </>
-        )
-      }
+      </div>
 
-
-
-      {game && (
-        <OfficeVisualization engineers={game?.engineers} sales={game?.sales_staff} />
-      )}
 
       {isGameOver && (
         <div className="text-red-500 mt-16">
@@ -202,34 +212,32 @@ export default function Home() {
         </div>
       )}
 
+      <div className="h-screen w-[50vw] justify-center flex items-center">
 
-      <div style={{ marginTop: 24 }}>
-        <h2>Quarter Decisions</h2>
+        <div className=" w-[75%] rounded-xl border border-[#ffffff46] flex flex-col items-center justify-center py-14">
+          <h2 className="text-center text-[22px] font-bold mb-12">Quarter Decisions</h2>
+          <div className="w-[50%]">
+            <p className='text-[16px] font-medium'>Unit Price</p>
+            <input type="number" name="price" id="price" className='h-9.5 w-full bg-transparent border-[1.65px] border-[#ffffff46] px-5 text-sm  outline-none focus:border-primary focus:border-b-[1.65px] ease-in-out duration-200 rounded-xl mb-8'
+              value={decisions.price} onChange={e => setDecisions({ ...decisions, price: Number(e.target.value) })} />
 
-        <label>
-          Unit Price
-          <input type="number" value={decisions.price} onChange={e => setDecisions({ ...decisions, price: Number(e.target.value) })} />
-        </label>
+            <p className='text-[16px] font-medium'>Hire Egineers</p>
+            <input type="number" name="engineers" id="engineers" className='h-9.5 w-full bg-transparent border-[1.65px] border-[#ffffff46] px-5 text-sm  outline-none focus:border-primary focus:border-b-[1.65px] ease-in-out duration-200 rounded-xl mb-8'
+              value={decisions.new_engineers} onChange={e => setDecisions({ ...decisions, new_engineers: Number(e.target.value) })} />
 
-        <label>
-          Hire Egineers
-          <input type="number" value={decisions.new_engineers} onChange={e => setDecisions({ ...decisions, new_engineers: Number(e.target.value) })} />
-        </label>
+            <p className='text-[16px] font-medium'>Hire Sales Staff</p>
+            <input type="number" name="engineers" id="engineers" className='h-9.5 w-full bg-transparent border-[1.65px] border-[#ffffff46] px-5 text-sm  outline-none focus:border-primary focus:border-b-[1.65px] ease-in-out duration-200 rounded-xl mb-8'
+              value={decisions.new_sales_staff} onChange={e => setDecisions({ ...decisions, new_sales_staff: Number(e.target.value) })} />
 
-        <label>
-          Hire Sales Staff
-          <input type="number" value={decisions.new_sales_staff} onChange={e => setDecisions({ ...decisions, new_sales_staff: Number(e.target.value) })} />
-        </label>
+            <p className='text-[16px] font-medium'>Salary (% of industry avg)</p>
+            <input type="number" name="engineers" id="engineers" className='h-9.5 w-full bg-transparent border-[1.65px] border-[#ffffff46] px-5 text-sm  outline-none focus:border-primary focus:border-b-[1.65px] ease-in-out duration-200 rounded-xl mb-12'
+              value={decisions.salary_pct} onChange={e => setDecisions({ ...decisions, salary_pct: Number(e.target.value) })} />
 
-        <label>
-          Salary (% of industry avg)
-          <input type="number" value={decisions.salary_pct} onChange={e => setDecisions({ ...decisions, salary_pct: Number(e.target.value) })} />
-        </label>
-
-        <button disabled={submitting || isGameOver || isWin} onClick={advanceQuarter} className="border rounded-xl py-2 px-4">
-          {isGameOver ? 'Game Over' : isWin ? 'You Win' : submitting ? 'Advancing...' : 'Advance Quarter'}
-        </button>
-
+            <button disabled={submitting || isGameOver || isWin} onClick={advanceQuarter} className="border rounded-xl py-2 px-4 w-full">
+              {isGameOver ? 'Game Over' : isWin ? 'You Win' : submitting ? 'Advancing...' : 'Advance Quarter'}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );

f73dfdb9503803f42001576797d5dda4929da1cf
Emmanuel Ochieng <eochieng16@gmail.com>
2026-03-02 12:19:41 +0300

    improve login UI

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,11 +17,26 @@ const LoginPage = () => {
     }
 
     return (
-        <div>
-            <input placeholder='email' onChange={e => setEmail(e.target.value)} />
-            <input placeholder='password' type='password' onChange={e => setPassword(e.target.value)} />
-            <button onClick={() => handleLogin(false)}>Login</button>
-            <button onClick={() => handleLogin(true)}>Sign up</button>
+        <div className='flex items-center justify-center h-screen'>
+            <div className=" w-[50%] rounded-xl border border-[#ffffff46] flex flex-col items-center justify-center py-14">
+                <h2 className="text-center text-[22px] font-bold mb-12">Account Management</h2>
+                <div className="w-[50%]">
+                    <p className='text-[16px] font-medium'>Email</p>
+                    <input placeholder='Email' type="email" name="email" id="email" className='h-9.5 w-full bg-transparent border-[1.65px] border-[#ffffff46] px-5 text-sm  outline-none focus:border-primary focus:border-b-[1.65px] ease-in-out duration-200 rounded-xl mb-8'
+                        value={email} onChange={e => setEmail(e.target.value)} />
+
+                    <p className='text-[16px] font-medium'>Password</p>
+                    <input placeholder='Password' type="password" name="password" id="password" className='h-9.5 w-full bg-transparent border-[1.65px] border-[#ffffff46] px-5 text-sm  outline-none focus:border-primary focus:border-b-[1.65px] ease-in-out duration-200 rounded-xl mb-8'
+                        value={password} onChange={e => setPassword(e.target.value)} />
+
+
+                    <div className="flex justify-center items-center gap-8">
+                        <button className="border rounded-xl py-2 px-4 w-1/3" onClick={() => handleLogin(false)}>Login</button>
+                        <button className="border rounded-xl py-2 px-4 w-1/3" onClick={() => handleLogin(true)}>Sign up</button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
